83dd102bb76bc44dd4f063cc6348df2e2ee00b4d
Locke vanUbor <vanUbor@live.de>
2019-12-07 23:11:18 +0100

    pull request commit

--- PlayersAndDices.TheDarkEye/Characters/Character.cs
+++ PlayersAndDices.TheDarkEye/Characters/Character.cs
@@ -7,5 +7,10 @@ namespace PlayersAndDices.TheDarkEye.Characters
     public class Character
     {
         public string Name { get; set; }
+
+        /// <summary>
+        /// pull request change
+        /// </summary>
+        public int ID { get; set; }
     }
 }
