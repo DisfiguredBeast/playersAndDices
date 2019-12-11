3772c1b8826d8c013dfad1096f0579b612f70284
Locke vanUbor <vanUbor@live.de>
2019-12-11 19:42:46 +0100

    doin some forms stuff

--- PlayersAndDices/ClientApp/src/app/the-dark-eye/weapons/ranged/edit-ranged-dialog/edit-ranged-dialog.ts
+++ PlayersAndDices/ClientApp/src/app/the-dark-eye/weapons/ranged/edit-ranged-dialog/edit-ranged-dialog.ts
@@ -1,7 +1,12 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { RangedWeapon } from '../ranged.component';
+import { FormsModule } from '@angular/forms';
 
+export interface Bow{
+  name : string,
+  description : string
+}
 
 @Component({
   selector: 'app-edit-ranged-dialog',
@@ -9,14 +14,23 @@ import { RangedWeapon } from '../ranged.component';
   styleUrls: ['./edit-ranged-dialog.css']
 })
 
+
 export class EditRangedDialog {
 
-  constructor (public dialogRef: MatDialogRef<EditRangedDialog>,
-  @Inject(MAT_DIALOG_DATA) public data: RangedWeapon) { 
+  ranged : Bow;
 
+  enteredData : any;
+  constructor (public dialogRef: MatDialogRef<EditRangedDialog>,
+        @Inject(MAT_DIALOG_DATA) public data: RangedWeapon) { 
+          this.ranged = {name:"Zauberbogen", description:"eine beschreibung halt"};
   }
 
   ngOnInit() {
+  }
+
+  onClose() : void {
+    this.enteredData = {name: "MOAB", range: 42, damage: 1337};
+    this.dialogRef.close(); 
   }
 
 }
