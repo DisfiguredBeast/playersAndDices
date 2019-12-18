c29278877f17aada08f83e30aafc0f05e87a72a0
Fabian Gutbrod <fabiangutbrod@web.de>
2019-12-18 14:12:15 +0100

    Start settings for adventures

--- PlayersAndDices/ClientApp/src/app/adventures/new-adventure/new-adventure.component.ts
+++ PlayersAndDices/ClientApp/src/app/adventures/new-adventure/new-adventure.component.ts
@@ -7,17 +7,26 @@ import { Component } from '@angular/core';
 })
 export class NewAdventureComponent {
 
-    rows: any[] = [];
-    activeCell: any;
+    $activeCell: any;
+
+    adventure: any = {
+        rows: []
+    };
 
     constructor() {
 
     }
 
     ngOnInit() {
-        this.rows.push({ cells: [{ fontSize: "32px", placeholder: "Title...", multiline: false, underline: true }] });
-        this.rows.push({ cells: [{ fontSize: "24px", placeholder: "Subtitle...", multiline: false }] });
-        this.rows.push({ cells: [{ fontSize: "14px", placeholder: "Text...", multiline: true }] });
-        this.rows.push({ cells: [{ fontSize: "14px", placeholder: "Text...", multiline: true }, { fontSize: "14px", placeholder: "Text...", multiline: true }] });
+        this.adventure.rows.push({ cells: [{ fontSize: "32px", placeholder: "Title...", multiline: false, underline: true }] });
+        this.adventure.rows.push({ cells: [{ fontSize: "24px", placeholder: "Subtitle...", multiline: false }] });
+        this.adventure.rows.push({ cells: [{ fontSize: "14px", placeholder: "Text...", multiline: true }] });
+        this.adventure.rows.push({ cells: [{ fontSize: "14px", placeholder: "Text...", multiline: true }, { fontSize: "14px", placeholder: "Text...", multiline: true }] });
+    }
+
+    resizeTextArea(event) {
+        let textArea = event.target;
+        textArea.style.height = "0px";
+        textArea.style.height = textArea.scrollHeight + "px";
     }
 }
